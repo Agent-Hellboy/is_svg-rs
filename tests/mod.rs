5651fe86522a90d7e7deebaf3012959107acf25a
bool <booleancoercion@gmail.com>
2020-12-22 09:46:42 +0200

    Implemented custom library error type, and changed is_svg return type to Result

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,16 +1,9 @@
-extern crate is_svg;
-
 #[test]
 fn test_png() {
-    assert_eq!(
-        is_svg::is_svg("./images/example.png"),
-        "please check imghdr to check this file"
-    );
+    assert_eq!(is_svg::is_svg("./images/example.png").unwrap(), false);
+    // should it be false? maybe this should be changed accordingly
 }
 #[test]
 fn test_jpeg() {
-    assert_eq!(
-        is_svg::is_svg("./images/example.svg"),
-        "yes it is a svg file"
-    );
+    assert_eq!(is_svg::is_svg("./images/example.svg").unwrap(), true);
 }
